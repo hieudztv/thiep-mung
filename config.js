8a1781faebd8d57a0430883ca8d6c0ddb3ed1ebd
hieudztv <92874634+hieudztv@users.noreply.github.com>
2021-10-20 23:03:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Em oanh xinh gái';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = 'http://oanhxinhgai.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc bff của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
